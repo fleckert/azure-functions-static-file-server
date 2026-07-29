021eb7f74f6bd495ca5d2fddcb06d2f92ffc736b
Florian Eckert <florian.eckert@gmail.com>
2026-07-29 06:39:21 +0200

    Program.cs - removed Azure Functions specific Urls setup (#1)

--- StaticFilesHandler/Program.cs
+++ StaticFilesHandler/Program.cs
@@ -1,18 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
-using System;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-var environmentVariableCustomHandlerPort = Environment.GetEnvironmentVariable("FUNCTIONS_CUSTOMHANDLER_PORT");
-
-if(!ushort.TryParse(environmentVariableCustomHandlerPort, out ushort port))
-{
-    throw new InvalidOperationException($"Environment Variable 'FUNCTIONS_CUSTOMHANDLER_PORT' is set to '{environmentVariableCustomHandlerPort}' and does not specify a valid port number.");
-}
-
-app.Urls.Add($"http://127.0.0.1:{port}");
 app.UseDefaultFiles(new DefaultFilesOptions { DefaultFileNames = { "index.html" } });
 app.UseStaticFiles();
 
